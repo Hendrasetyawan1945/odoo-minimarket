9f9194547c7e719fa1d6856a66f8fb41d96a9a73
hendrasetyawan <hendrasetyawan1945@gmail.com>
2022-08-23 20:20:05 +0700

    pindah rumah

--- addonsku/minimarketku/models/pembelian_detail.py
+++ addonsku/minimarketku/models/pembelian_detail.py
@@ -14,6 +14,10 @@ class pembelian_detail(models.Model):
         inverse_name='kode_barang',
         string='Kode_barang_ids',
         required=False)
+    nama_barangpembelian = fields.Char(
+        compute="_compute_namabarang",
+        string='Nama_barang',
+        required=False)
     harga_beli = fields.Integer(
         compute="_compute_hargabeli",
         string='Harga_beli',
@@ -30,5 +34,10 @@ class pembelian_detail(models.Model):
     def _compute_hargabeli(self):
         for a in self:
             a.harga_beli = a.kode_barang_ids.harga_beli
+
+    @api.depends('nama_barangpembelian')
+    def _compute_namabarang(self):
+        for a in self:
+            a.nama_barangpembelian = a.kode_barang_ids.nama_barang
 
 
